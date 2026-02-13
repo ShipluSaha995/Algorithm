cef8c40667803c27986a79f482acb339e864503b
ShipluSaha995 <shiplusaha995@gmail.com>
2026-02-13 12:51:42 +0600

    updated

--- Graph.java
+++ Graph.java
@@ -447,3 +447,83 @@ Undirected Path : Edges can be traversed in both ways
 0-1-2-3-5-6
 
 */
+
+//BFS 
+
+/*
+BFS(Breadth First Serch) is a graph traversal algorithm that explores vertices levle by levle. It first 
+visits all neighbours of a starting node then neighbors of those neighbors and so on.
+
+key points : 
+1. It uses QUEUE 
+2. works for both undirected and directed graph
+3. finds the shortest path in an directed graph
+4. Before prcessing all vertices, insert the adjacency vertices in the queue.
+5. do not insert visited vertices again 
+6. do no repeat or reinsert alresdy inserted vertices
+
+
+*/
+
+import java.util.*;
+
+class BFS{
+    int V;
+    ArrayList<ArrayList<Integer>>adj;
+    
+    BFS(int V){
+        this.V=V;
+        adj=new ArrayList<>();
+        
+        for(int i=0; i<V; i++){
+            adj.add(new ArrayList<>());
+
+        }
+    }
+
+    void addEdge(int u, int v){
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+
+    void bfs(int start){
+        boolean[] visited = new boolean[V];
+        Queue<Integer>queue=new ArrayDeque<>();
+
+        visited[start]=true;
+        queue.add(start);
+
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            System.out.print(node+" ");
+            
+            for(int neighbor:adj.get(node)){
+                if(!visited[neighbor]){
+                    visited[neighbor]=true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+}
+
+public class Graph{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int V = sc.nextInt();
+        int E = sc.nextInt();
+
+        BFS b = new BFS(V);
+
+        for(int i =0; i<E; i++){
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+
+            b.addEdge(u,v);
+        }
+
+        int start = sc.nextInt();
+        b.bfs(start);
+    }
+}
